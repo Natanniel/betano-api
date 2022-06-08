b94701908236f50a1457abcc3ad615766f269888
Natanniel <natanniel.alves@outlook.com>
2022-06-08 15:28:24 -0300

    deploy

--- src/controllers/estrategias.js
+++ src/controllers/estrategias.js
@@ -2,18 +2,28 @@
 class Estrategia {
 
     async cadastrar(req, res) {
-        let Estrategia = require('../models/estrategia');       
-        let estrategia = new Estrategia(req.body);
-        await estrategia.save();
+        let Estrategia = require('../models/estrategia');
+
+        if (req.body.id == "") {
+            let estrategia = new Estrategia(req.body);
+            await estrategia.save();
+        }else{
+            await Estrategia.updateOne({_id: req.body.id},req.body)           
+        }
         res.send();
     }
 
     async estrategia(req, res) {
-        let Estrategia = require('../models/estrategia');       
-        let estrategias = await Estrategia.find({status : 1});
+        let Estrategia = require('../models/estrategia');
+        let estrategias = await Estrategia.find({ status: 1 });
         res.send(estrategias);
     }
 
+    async estrategiaID(req, res) {
+        let Estrategia = require('../models/estrategia');
+        let estrategias = await Estrategia.findOne({ _id: req.params.id, status: 1 });
+        res.send(estrategias);
+    }
 
 }
 
--- src/routes/estrategias.js
+++ src/routes/estrategias.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Estrategia = require('../controllers/estrategias');
 
 router.post('/estrategia/cadastro', Estrategia.cadastrar);
-router.get('/estrategia', Estrategia.estrategia)
+router.get('/estrategia', Estrategia.estrategia);
+router.get('/estrategia/:id', Estrategia.estrategiaID)
 
 module.exports = router; 
